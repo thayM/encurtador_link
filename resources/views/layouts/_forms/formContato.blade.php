98d2ef0e9270c8c8ce4c5e131e0dedab2d84e3f4
thayM <thay.contato1@gmail.com>
2024-05-20 16:20:03 -0300

    css index

--- resources/views/layouts/_forms/formContato.blade.php
+++ resources/views/layouts/_forms/formContato.blade.php
@@ -1,21 +1,31 @@
 {{ $slot }}
 <form action="#" method="POST" class="{{ $css }}">
     @csrf
-    <label for="">E-mail</label>
-    <input type="text" id="email" name="email">
 
-    <label for="">Nome</label>
-    <input type="text" id="nome" name="nome">
+    <div class="campos">
+    <label class="{{ $cssLabel }}" for="">E-mail</label>
+    <input class="{{ $cssInput }}" type="text" id="email" name="email">
+    </div>
 
-    <label for="">Mensagem</label>
-    <input type="text" id="mensagem" name="mensagem">
+    <div class="campos">
+    <label class="{{ $cssLabel }}" for="">Nome</label>
+    <input class="{{ $cssInput }}" type="text" id="nome" name="nome">
+    </div>
 
-    <label for="">Motivo</label>
-    <select name="motivo" id="motivo">
+    <div class="campos">
+    <label class="{{ $cssLabel }}" for="">Mensagem</label>
+    <input class="{{ $cssInput }}" type="text" id="mensagem" name="mensagem">
+    </div>
+
+    <div class="campos">
+    <label class="{{ $cssLabel }}" for="">Motivo</label>
+    <select class="{{ $cssSelect }}" name="motivo" id="motivo">
+        <option value="">Selecione um motivo...</option>
         <option value="1">Reclamação</option>
         <option value="2">Dúvida</option>
         <option value="3">Elogio</option>
     </select>
+    </div>
 
     <button type="submit" class="{{$classBtn}}">Enviar</button>
 </form>
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -14,13 +14,27 @@
     @endcomponent
 </nav>
 
-
+<div class="bodyCompleto">
 <div class="container">
 <img class="imgWelcome" src="{{ asset('img/welcome_cats.svg') }}" alt="">
 </div>
-    @component('layouts._forms.formContato', ['css' => 'formContato', 'classBtn' => 'btnEnviar'])
-    @endcomponent
 
+<div class="container2">
+<div class="Inferior">
+<div class="imagemForm">
+    <h1>Entre em contato conosco</h1>
+    <img class="imgContato" src="{{ asset('img/undraw_personal_email_re_4lx7.svg') }}" alt="">
+</div>
+
+<div class="formulario">
+    @component('layouts._forms.formContato', ['css' => 'formContato', 'classBtn' => 'btnEnviar', 'cssLabel' => 'titulosInput', 'cssInput' => 'inputs', 'cssSelect' => 'select'])
+    @endcomponent
+</div>
+</div>
+
+</div>
+
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
